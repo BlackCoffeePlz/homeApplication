9d4a9806d1cad6901eba9ef6c3fdce3390f25a1e
6lackwater <tcray.dev@gmail.com>
2024-02-03 10:14:06 -0600

    Initial commit.

---
+++ homeApplication/grails-app/init/homeApplication/BootStrap.groovy
@@ -0,0 +1,9 @@
+package homeApplication
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ homeApplication/src/integration-test/groovy/homeApplication/HomeApplicationSpec.groovy
@@ -0,0 +1,22 @@
+package homeApplication
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+
+import geb.spock.*
+
+/**
+ * See https://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+@Rollback
+class HomeApplicationSpec extends GebSpec {
+
+    void "test something"() {
+        when:"The home page is visited"
+            go '/'
+
+        then:"The title is correct"
+            title == "Welcome to Grails"
+    }
+
+}
